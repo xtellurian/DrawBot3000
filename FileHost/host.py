ef76692154dd1f4acb74f9771b1ab3b0153f6fa4
Rian Finnegan <flanagan89@gmail.com>
2015-07-11 11:08:32 +1000

    added some control code

--- FileHost/host.py
+++ FileHost/host.py
@@ -1,15 +1,42 @@
 import serial
 import sys
+import polargraph
+
+#consts
+OK = "READY"
 	
-#user = serial.Serial('/dev/tty.robot something', 57600)
+	
 
+#get serial port
+ser = serial.Serial('/dev/tty.usbmodem1411', 57600)
+commands = polargraph.commands()
+
+#get file data 
 [filename] = sys.argv[1:]
-
 with open(filename,'r') as datafile:
 	data = datafile.read()
-	
+ #convert to useful data
 data = str.splitlines(data)
 print str(len(data)) + " commands in file"
+
+# funcs
+def Next(commandcount):
+	return data[commandcount]
+
+count = 0
+while True:
+	response = ser.readline()
+	print "From Bot: " + response
+	if OK in response:
+		send = Next(count)
+		count = count + 1
+		print send
+		ser.write(send)
+		
+	
+
+
+	
 
 
 
